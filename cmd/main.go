330e0a781c9556389f49966443173835eb91f19e
RewasReda <rewasreda2015@gmail.com>
2020-10-31 15:09:16 +0200

    adding json output

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import ("github.com/RewasReda/Vanerabilities-Scanner")
+
+
+func main() {
+	fmt.Println("Hello!")
+
+}
--- cmd/scan.go
+++ cmd/scan.go
@@ -22,7 +22,7 @@ func scan(Contain string) {
 	app := internal.NewApp(version)
 	//err := app.Run(os.Args)
 	fmt.Println(os.Args)
-	theArray := []string{"", Contain}
+	theArray := []string{"", "image", "-f", "json", Contain}
 	// theArray[0] = "/tmp/go-build538054758/b001/exe/main"
 	// theArray[1] = "mongo"
 	// theArray[2] = "mongo"
